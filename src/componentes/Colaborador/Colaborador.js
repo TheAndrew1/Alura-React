0efead18f8100d17c3b38eb2d0a786bba003a6a1
Andre <andre.velasco@itaipuparquetec.org.br>
2025-01-27 18:04:13 -0300

    card colaborador base

---
+++ src/componentes/Colaborador/Colaborador.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/TheAndrew1.png' alt='André Velasco'/>
+            </div>
+            <div className='rodape'>
+                <h4>André Velasco</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
