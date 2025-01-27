d2c756811b0317d016435001bb57798e73c5b46d
Andre <andre.velasco@itaipuparquetec.org.br>
2025-01-27 17:29:14 -0300

    base do componente de times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
+import Time from './componentes/Time/Time';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
@@ -13,6 +14,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoCadastrar={colaborador => novoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
